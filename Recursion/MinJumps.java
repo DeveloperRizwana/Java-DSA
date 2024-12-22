cab3e0d3b3844a8cc75a9f30daa9083e11984fb2
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-22 19:44:21 +0530

    learned

---
+++ Recursion/MinJumps.java
@@ -0,0 +1,10 @@
+public class MinJumps {
+    static int minJumps(int[] arr) {
+        // code here
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        
+    }
+    
+}
