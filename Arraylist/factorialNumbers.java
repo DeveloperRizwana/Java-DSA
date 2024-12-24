7a1b66ae5e15fbf3f6a72bfdac19d7305ae6ad1d
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-24 08:57:56 +0530

    practiced

--- Arraylist/factorialNumbers.java
+++ Arraylist/factorialNumbers.java
@@ -15,6 +15,7 @@ public class factorialNumbers {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
         long n = sc.nextInt();
         System.out.println(factorialNumbers(n));
         
