8e1b80b6e9fd7996235b2054e3e6e7bae2e9a593
DevloperRizwana <dev.rizwana.perween01@gmail.com>
2024-12-24 09:15:42 +0530

    practiced

---
+++ Array/reversArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class reversArray {
+    public  void reverseArray(int arr[]) {
+        int st = 0, end = arr.length-1;
+        while (st < end) {
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            end--;
+            st++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 2, 6, 5};
+        reversArray reverse = new reversArray();
+        reverse.reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
