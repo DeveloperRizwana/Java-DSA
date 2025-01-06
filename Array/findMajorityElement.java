a4c06398afc4f52d20c2940d179dd59bf5b524ea
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-06 12:40:26 +0530

    practiced

---
+++ Array/findMajorityElement.java
@@ -0,0 +1,21 @@
+public class findMajorityElement {
+    public static int majorityElement(int[] nums) {
+        int count = 0, number = nums[0];
+        for(int i = 0; i<nums.length; i++){
+            if(count == 0){
+                number = nums[i];
+            }
+            if(nums[i] == number){
+                count++;
+ 
+            } else {
+                count--;
+            }
+        }
+        return number;
+    }
+    public static void main(String[] args) {
+        int[] arr = {6,5,5};
+        System.out.println(majorityElement(arr));
+    }
+}
