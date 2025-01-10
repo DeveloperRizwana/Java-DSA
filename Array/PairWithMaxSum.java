049546003043b740367748b17521298d2cc205e3
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-10 17:45:34 +0530

    practiced problem on geek for geek

---
+++ Array/PairWithMaxSum.java
@@ -0,0 +1,27 @@
+public class PairWithMaxSum {
+    public static int pairWithMaxSum(int arr[]) {
+        int maxScore = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length-1; i++){
+            int min = Integer.MAX_VALUE;
+            int secondmin = Integer.MAX_VALUE;
+            for(int j = i; j<arr.length; j++){
+                if(arr[j] < min) {
+                    secondmin = min;
+                    min = arr[j];
+                } else if(arr[j] > min && arr[j] < secondmin){
+                    secondmin = arr[j];
+                } 
+                // if(maxScore != Integer.MAX_VALUE){
+                    int minScore = min + secondmin;
+                    maxScore = Math.max(maxScore, minScore);
+                // }
+            }
+        }
+        return maxScore;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 1, 6};
+        System.out.println(pairWithMaxSum(arr));
+    }
+    
+}
