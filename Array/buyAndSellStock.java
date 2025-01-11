7057ebe003dabfb332244f159f5c713f462acec6
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-11 11:40:38 +0530

    practiced

---
+++ Array/buyAndSellStock.java
@@ -0,0 +1,18 @@
+public class buyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int buy = Integer.MAX_VALUE;
+        int sell = Integer.MIN_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]);
+            // sell = Math.max(sell, prices[i]);
+            profit = Math.max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2 };
+        System.out.println(maxProfit(a));
+    }
+}
