0e9cdea5fe6cb1c8db0ba7182f4de1270b1f262e
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-17 11:05:17 +0530

    practiced

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,33 +1,19 @@
 public class fifthPractcie {
-    public static void swap(int[] nums, int st, int end){
-        int temp = nums[st];
-        nums[st] = nums[end];
-        nums[end] = temp;
-    }
-    public static void sortColors(int[] nums) {
-        int st = 0, end = nums.length-1;
-        while(st<=end){
-           if(nums[st] == 0){
-            st++;
-           } else if(nums[end] == 2){
-            end--;
-           } else if(nums[st] != 0 && nums[end] == 0){
-            swap(nums, st, end);
-            st++;
-           } else if(nums[st] == 2 && nums[end] != 2){
-            swap(nums, st, end);
-            end--;
-           } else {
-            st++;
-           }
+    static int evenlyDivides(int n) {
+        int reversed = 0;
+        while (n != 0) {
+            int d = n % 10;
+            n = n / 10;
+            if (reversed > Integer.MAX_VALUE / 10 || reversed < Integer.MIN_VALUE / 10) {
+                // Prevent integer overflow
+                return 0;
+            }
+            reversed = reversed * 10 + d;
         }
-        
+        return reversed;
     }
     public static void main(String[] args) {
-        int[] nums = {1,0,1};
-        sortColors(nums);
-        for(int i = 0; i < nums.length; i++){
-            System.out.print(nums[i] + " ");
-        }
+       int n = -123;
+       System.out.println(evenlyDivides(n));
     }
 }
