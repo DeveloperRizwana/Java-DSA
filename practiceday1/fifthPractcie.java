03659f7aefc3c1230272a08e9dae72a52100d298
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-22 19:45:21 +0530

    practiced on geeksforgeeks

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,14 +1,14 @@
 public class fifthPractcie {
     static int countDig(int n) {
-        int sum = 0;
+        int totalSum = 0;
         for(int i = 1; i <= n; i++) {
            for(int j = 1; j<=n; j++){
             if(i%j == 0){
-                sum += j;
+                totalSum += j;
             }
            }
         }
-        return sum;
+        return totalSum;
     }
     
     public static void main(String[] args) {
