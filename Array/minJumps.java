4ef26c1731f7b2b555bc8548d9c1d3a6035d1399
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-23 10:08:52 +0530

    practiced on gfg

---
+++ Array/minJumps.java
@@ -0,0 +1,31 @@
+public class minJumps {
+    static int findMinJumps(int[] arr) {
+        if(arr.length == 0){
+            return -1;
+        }
+        if (arr[0] == 0) {
+            return -1;
+        }
+        int maxRange = 0, lastIdx = 0, jumps = 0;
+        for(int i = 0; i < arr.length-1; i++) {
+            maxRange = Math.max(maxRange, i+arr[i]);
+            if(i == lastIdx){
+                if(maxRange==i){
+                    jumps = -1;
+                    break;
+                }
+                lastIdx = maxRange;
+                jumps++;
+                if(maxRange >= arr.length-1){
+                    break;
+                }
+            }
+        }
+        return jumps;
+    }
+    public static void main(String[] args) {
+        int[] a = {1,4,3,2,6,7};
+        System.out.println("Minimum jump is : " +findMinJumps(a));
+    }
+    
+}
--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -3,21 +3,29 @@ public class fifthPractcie {
         if(arr.length == 0){
             return -1;
         }
-        if(arr[0] == 0) return -1;
-        int count = 0;
-        for(int i = 0; i < arr.length-1; i++) {
-            if(arr[i] == 0){
-                break;
-            } 
-            int jump = arr[i];
-            i += jump;
-            count++;
+        if (arr[0] == 0) {
+            return -1;
         }
-        return count;
+        int maxRange = 0, lastIdx = 0, jumps = 0;
+        for(int i = 0; i < arr.length-1; i++) {
+            maxRange = Math.max(maxRange, i+arr[i]);
+            if(i == lastIdx){
+                if(maxRange==i){
+                    jumps = -1;
+                    break;
+                }
+                lastIdx = maxRange;
+                jumps++;
+                if(maxRange >= arr.length-1){
+                    break;
+                }
+            }
+        }
+        return jumps;
     }
     
     public static void main(String[] args) {
-        int[] a = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        int[] a = {1,4,3,2,6,7};
         System.out.println("Answer is : " + countDig(a));
     }
 }
