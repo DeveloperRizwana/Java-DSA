e5524eac090ddee51c44a17c753fec455ea7f222
devRiz <dev.rizwana.perween01@gmail.com>
2025-01-28 20:08:06 +0530

    practiced

--- Recursion/towerOfHonai.java
+++ Recursion/towerOfHonai.java
@@ -1,20 +1,22 @@
 public class towerOfHonai {
-    static void printTowerOfHanoi(int n, char source, char auxiliary, char destination){
-        if(n == 0) return;
+    static void printTowerOfHanoi(int n, String source, String auxiliary, String destination) {
+        if (n == 0)
+            return;
 
-        if(n == 1){
-            System.out.println(source + "" + destination);
+        if (n == 1) {
+            System.out.println(source + " " + destination);
             return;
         }
 
-        printTowerOfHanoi(n-1, source, destination, auxiliary);
-        System.out.println(source + "" + destination);
-        printTowerOfHanoi(n-1, auxiliary, source, destination);
+        printTowerOfHanoi(n - 1, source, destination, auxiliary);
+        System.out.println(source + " " + destination);
+        printTowerOfHanoi(n - 1, auxiliary, source, destination);
     }
-      public static void main(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("Tower of hanoi: ");
-        int n = 1;
-        printTowerOfHanoi(n, 'a', 'b', 'c');
+        int n = 5;
+        printTowerOfHanoi(n, "left", "mid", "right");
     }
-    
+
 }
