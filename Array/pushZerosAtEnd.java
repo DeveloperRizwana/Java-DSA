c243c122d4ecfa6d54f43cab2ed724ab59688ae4
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-04 09:42:19 +0530

    practiced on leet code

--- Array/pushZerosAtEnd.java
+++ Array/pushZerosAtEnd.java
@@ -1,7 +1,6 @@
-import java.util.*;
+import java.util.Scanner;
 public class pushZerosAtEnd {
     static void findZerosAtEnd(int[] arr){
-        
         // int i = 0, j = arr.length - 1; 
         // while(i <= j){
         //     if(arr[i] == 0 && arr[j] != 0){
@@ -18,8 +17,28 @@ public class pushZerosAtEnd {
         //         j--;
         //     }
         // }
+
+        // 2nd approach
+//         if(nums.length == 0) return;
+//     int zeroes = 0;
+//     int n = nums.length;
+//     for(int i = 0; i < n; i++){
+//       if(nums[zeroes] == 0 && nums[i] != 0){
+//         swap(nums, zeroes, i);
+//         zeroes++;
+//       } else if(nums[zeroes] != 0){
+//         zeroes++;
+//       }
+//     }
+//   }
+//   public static void swap(int[] nums, int i, int j){
+//       int temp = nums[i];
+//       nums[i] = nums[j];
+//       nums[j] = temp;
+//   }
+
         int t = 0;    // optimized code
-        for(int i = 0; i < arr.length - 1; i++){
+        for(int i = 0; i < arr.length; i++){
             if(arr[i] != 0){
                 int temp = arr[i];
                 arr[i] = arr[t];
@@ -43,8 +62,6 @@ public class pushZerosAtEnd {
             arr[i] = sc.nextInt();
         }
         findZerosAtEnd(arr);
-        
-
     }
     
 }
