acd2dc41820ca6c2678629702883c8d179dd1395
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-04 09:46:16 +0530

    practiced

--- Array/rotateInPlace.java
+++ Array/rotateInPlace.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class rotateInPlace {
 
@@ -46,9 +46,6 @@ public class rotateInPlace {
         }
         return ans;
     }
-
-    
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array: ");
