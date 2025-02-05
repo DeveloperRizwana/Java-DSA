82a5f40d3bd9a52b9069d672c4cd1bbd8c991c70
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-05 08:29:23 +0530

    practiced on gfg

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,25 +1,14 @@
 public class fifthPractcie {
-  public static void rotate(int[] nums, int k) {
-    int n = nums.length;
-    k = k % n;
-    reverse(nums, 0, n-1);
-    reverse(nums, 0, k-1);
-    reverse(nums, k, n-1);
-  }
-  public static void reverse(int[] nums, int i, int j){
-    while (i<j) {
-      int temp = nums[i];
-      nums[i] = nums[j];
-      nums[j] = temp;
-      i++;
-      j--;
+  static boolean searchInSorted(int arr[], int k) {
+    for(int i = 0; i <= arr.length-1; i++){
+      if(arr[i] == k){
+        return true;
+      } 
     }
+    return false;
   }
   public static void main(String[] args) {
-    int[] a = {1,2,3,4,5,6,7};
-    rotate(a, 3);
-    for(int i = 0; i < a.length; i++){
-      System.out.print(a[i] + " ");
-    }
+    int[] a = {2, 3, 5, 6};
+    System.out.println(searchInSorted(a, 1));
   }
 }
