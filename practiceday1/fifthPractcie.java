ebabfa4cc4e84c5b92272139dec3fb5bedd72b54
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-06 08:57:43 +0530

    practiced on gfg

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,14 +1,41 @@
+import java.util.ArrayList;
+
 public class fifthPractcie {
-  static boolean searchInSorted(int arr[], int k) {
-    for(int i = 0; i <= arr.length-1; i++){
-      if(arr[i] == k){
-        return true;
+  public static ArrayList<Integer> findUnion(int a[], int b[]) {
+    int n = a.length, m = b.length;
+    ArrayList<Integer> result = new ArrayList<>();
+    int i = 0, j = 0;
+    while(i<n && j<m){
+      if(a[i] <= b[j]){
+        if(result.size() == 0 || result.get(result.size() - 1)  != a[i]){
+          result.add(a[i]);
+        }
+        i++;
+      } else {
+        if(result.size() == 0 || result.get(result.size() - 1) != b[j]){
+          result.add(b[j]);
+        }
+        j++;
       } 
     }
-    return false;
-  }
+    while(i<n){
+      if(result.size() == 0 || result.get(result.size() - 1) != a[i]){
+        result.add(a[i]);
+      }
+      i++;
+    }
+    while (j<m) {
+      if(result.size() == 0 || result.get(result.size() - 1)  != b[j]){
+        result.add(b[j]);
+      }
+      j++;
+    }
+    return result;
+    }
   public static void main(String[] args) {
-    int[] a = {2, 3, 5, 6};
-    System.out.println(searchInSorted(a, 1));
+    int[] a = {1,1,1,1,1};
+    int[] b = {2,2,2,2,2};
+    ArrayList<Integer> result = findUnion(a, b);
+    System.out.println(result);
   }
 }
