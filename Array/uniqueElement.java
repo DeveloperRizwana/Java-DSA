692ab02d57c579c485dce83bf6b9d2a26fcb3045
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-09 08:45:19 +0530

    practiced

--- Array/uniqueElement.java
+++ Array/uniqueElement.java
@@ -1,23 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
 public class uniqueElement {
     static int findUniqueElement(int[] nums) {
+        // approach-1
+        if(nums.length == 1 && nums[0] == -1) return -1;
         int n = nums.length;
+        int singleNum = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    nums[i] = -1;
-                    nums[j] = -1;
-                }
+          for (int j = i+1; j < n; j++) {
+            if (nums[i] == nums[j]) {
+              nums[i] = 0;
+              nums[j]= 0;
             }
+          }
+          if (nums[i] != 0) {
+            singleNum = nums[i];
+          }
         }
-        int ans = -1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                ans = nums[i];
-            }
-        }
-        return ans;
+        return singleNum;
 
+        // // approach-2
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             nums[i] = -1;
+        //             nums[j] = -1;
+        //         }
+        //     }
+        // }
+        // int ans = -1;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > 0) {
+        //         ans = nums[i];
+        //     }
+        // }
+        // return ans;
+
+        //approach-3
         // int n = nums.length;
         // int[] result = new int[n];
         // for(int i = 0; i < n; i++){
