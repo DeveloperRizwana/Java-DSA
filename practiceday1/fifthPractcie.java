6b22c2131227d482d4d2a75065e8d1fb46457adb
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-09 22:38:16 +0530

    practiced

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,24 +1,10 @@
 public class fifthPractcie {
   public static int singleNumber(int[] nums) {
-    if(nums.length == 1 && nums[0] == -1) return -1;
-    int n = nums.length;
-    int singleNum = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i+1; j < n; j++) {
-        if (nums[i] == nums[j]) {
-          nums[i] = 0;
-          nums[j]= 0;
-        }
-      }
-      if (nums[i] != 0) {
-        singleNum = nums[i];
-      }
-    }
-    return singleNum;
+    
   }
 
   public static void main(String[] args) {
-    int[] a = { 1};
+    int[] a = { 1,0,1};
     System.out.println(singleNumber(a));
   }
 }
