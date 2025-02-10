fab7025f6f1d0e069273517368af31f7fec92abc
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-10 22:45:23 +0530

    practiced on gfg

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,10 +1,22 @@
 public class fifthPractcie {
-  public static int singleNumber(int[] nums) {
-    
+  public static int longestSubarray(int[] nums, int k) {
+    int maxLength = 0;
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      int  sum = 0;
+      for (int j = i; j < n; j++) {
+        sum += nums[j];
+        if (sum == k) {
+          maxLength = Math.max(maxLength, (j - i) + 1);
+        }
+      }
+    }
+    return maxLength;
   }
 
   public static void main(String[] args) {
-    int[] a = { 1,0,1};
-    System.out.println(singleNumber(a));
+    int[] a = { 94, -33, -13, 40, -82, 94, -33, -13, 40, -82
+    };
+    System.out.println(longestSubarray(a, 52));
   }
 }
