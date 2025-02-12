e5d68b998b535f2263318204300beb1f722ee07c
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-12 12:03:42 +0530

    modified

--- hashMap/mostFrequentElement.java
+++ hashMap/mostFrequentElement.java
@@ -3,26 +3,33 @@ import java.util.HashMap;
 public class mostFrequentElement {
     public static void main(String[] args) {
         int[] arr = { 1, 1, 2, 3, 4, 1, 2, 2, 2 };
-        HashMap<Integer, Integer> fre = new HashMap<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
         for (int e : arr) {
-            if (!fre.containsKey(e)) {
-                fre.put(e, 1);
+            if (!freq.containsKey(e)) {
+                freq.put(e, 1);
             } else {
-                fre.put(e, fre.get(e) + 1);
+                freq.put(e, freq.get(e) + 1);
             }
         }
         System.out.println("Frequency Map");
-        System.out.println(fre.entrySet());
+        System.out.println(freq.entrySet());
 
-        int maxFre = -1, ansKey = -1;
-        for (var e : fre.entrySet()) {
-            if (e.getValue() > maxFre) {
-                maxFre = e.getValue();
-                ansKey = e.getKey();
+        int maxFreq = -1, ansKey = -1;
+        // for (var e : freq.entrySet()) {
+        //     if (e.getValue() > maxFreq) {
+        //         maxFreq = e.getValue();
+        //         ansKey = e.getKey();
+        //     }
+        // }
+
+        for(var key : freq.keySet()){
+            if(freq.get(key) > maxFreq){
+                maxFreq = freq.get(key);
+                ansKey = key;
             }
         }
         // System.out.println(maxFre);
-        System.out.printf("%d has max frequency and it occurs %d times", ansKey, maxFre);
+        System.out.printf("%d has max frequency and it occurs %d times", ansKey, maxFreq);
     }
 }
 
