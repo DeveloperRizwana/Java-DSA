70d22081323e0ac4847a888dde8b7bec3cc16dc1
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-17 09:15:42 +0530

    practiced on gfg

--- hashMap/longestSubarraySum.java
+++ hashMap/longestSubarraySum.java
@@ -7,21 +7,19 @@ public class longestSubarraySum {
         for (int i = 0; i < arr.length; i++) {
             prefSum += arr[i];
 
-            // if(prefSum == k){
-            // maxLen = i+1;
-            // }
+            if(prefSum == k) maxLen = i+1;
 
             if (mp.containsKey(prefSum - k)) {
                 maxLen = Math.max(maxLen, (i - mp.get(prefSum - k)));
             } else {
-                mp.put(prefSum, i);
+                mp.putIfAbsent(prefSum, i);
             }
         }
         return maxLen;
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 5, 2, 7, 1, -10 };
-        System.out.println(longestSubarray(arr, 15));
+        int[] arr = { -5, 8, -14, 2, 4, 12, };
+        System.out.println(longestSubarray(arr, -5));
     }
 }
