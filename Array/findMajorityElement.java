34eca8d88aaf9f073d43199b6f893e7680536302
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-19 11:36:46 +0530

    practiced a problem on leet code

--- Array/findMajorityElement.java
+++ Array/findMajorityElement.java
@@ -1,21 +1,33 @@
+import java.util.HashMap;
+
 public class findMajorityElement {
     public static int majorityElement(int[] nums) {
-        int count = 0, number = nums[0];
-        for(int i = 0; i<nums.length; i++){
-            if(count == 0){
-                number = nums[i];
-            }
-            if(nums[i] == number){
-                count++;
- 
+        // int count = 0, number = nums[0];
+        // for(int i = 0; i<nums.length; i++){
+        //     if(count == 0){
+        //         number = nums[i];
+        //     }
+        //     if(nums[i] == number){
+        //         count++;
+        //     } else {
+        //         count--;
+        //     }
+        // }
+        // return number;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(!mp.containsKey(nums[i])){
+                mp.put(nums[i], 1);
             } else {
-                count--;
+                mp.put(nums[i], mp.get(nums[i]) +1);
             }
+            if(mp.get(nums[i]) > n/2) return nums[i];
         }
-        return number;
+        return 0;
     }
     public static void main(String[] args) {
-        int[] arr = {2,4,4,2,4};
+        int[] arr = {3,2,3};
         System.out.println("The majority element is : " + majorityElement(arr));
     }
 }
