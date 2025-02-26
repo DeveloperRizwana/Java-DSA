68934e17be6d20277afa3e2fa73d30388da6da7c
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-26 10:08:51 +0530

    practiced

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,26 +1,47 @@
 import java.util.Arrays;
 
 public class fifthPractcie {
-  public static int[] rearrangeArray(int[] nums) {
-    int n = nums.length;
-    int[] result = new int[n];
-    int j = 0, k = 1;
-    for (int i = 0; i < n; i++) {
-      if (nums[i] > 0) {
-        result[j] = nums[i];
-        j += 2;
-      } else {
-        result[k] = nums[i];
-        k += 2;
+  public static void nextPermutation(int[] nums) {
+    int n = nums.length, idx = -1;
+    for (int i = n - 1; i > 0; i--) {
+      if (nums[i] > nums[i - 1]) {
+        idx = i - 1;
+        break;
       }
     }
-    return result;
+    if (idx == -1) {
+      reverse(nums, 0, n - 1);
+    }
+    for (int i = n - 1; i > idx; i--) {
+      if (nums[i] > nums[idx]) {
+        swap(nums, i, idx);
+        break;
+      }
+    }
+    reverse(nums, idx + 1, n - 1);
+
+  }
+
+  public static void reverse(int[] a, int i, int j) {
+    while (i > j) {
+      swap(a, i, j);
+      i++;
+      j--;
+    }
+  }
+
+  public static void swap(int[] a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
   }
 
   public static void main(String[] args) {
-    int[] a = { 3, 1, -2, -5, 2, -4 };
-    int[] result = rearrangeArray(a);
-    System.out.println("Ans");
-    System.out.println("Arranged Array is : " + Arrays.toString(result));
+    int[] a = { 3, 2, 1 };
+    nextPermutation(a);
+    // for(int i = 0; i < a.length; i++){
+    // System.out.print(a[i] + " ");
+    // }
+    System.out.println(Arrays.toString(a));
   }
 }
