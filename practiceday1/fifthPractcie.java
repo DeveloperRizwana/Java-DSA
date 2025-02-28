881ad45f03a32d4c01c2ada70209c859c6e967cb
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-02-28 08:44:35 +0530

    practiced on gfg

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,48 +1,24 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class fifthPractcie {
-  public static void nextPermutation(int[] nums) {
-    int n = nums.length, idx = -1;
-    for (int i = n - 1; i > 0; i--) {
-      if (nums[i] > nums[i - 1]) {
-        idx = i - 1;
-        break;
+  static ArrayList<Integer> leaders(int arr[]) {
+    int n = arr.length, leaders = arr[n - 1];
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int i = n - 1; i >= 0; i--) {
+      if (arr[i] >= leaders) {
+        leaders = arr[i];
+        result.add(arr[i]);
       }
     }
-    if (idx == -1) {
-      reverse(nums, 0, n - 1);
-      return;
-    }
-    for (int i = n - 1; i > idx; i--) {
-      if (nums[i] > nums[idx]) {
-        swap(nums, i, idx);
-        break;
-      }
-    }
-    reverse(nums, idx + 1, n - 1);
-
-  }
-
-  public static void reverse(int[] a, int i, int j) {
-    while (i < j) {
-      swap(a, i, j);
-      i++;
-      j--;
-    }
-  }
-
-  public static void swap(int[] a, int i, int j) {
-    int temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+    Collections.reverse(result);
+    return result;
   }
 
   public static void main(String[] args) {
-    int[] a = { 1,2,3,4,5 };
-    nextPermutation(a);
-    // for(int i = 0; i < a.length; i++){
-    // System.out.print(a[i] + " ");
-    // }
-    System.out.println(Arrays.toString(a));
+    int[] a = { 5, 10, 20, 40 };
+    ArrayList<Integer> result = leaders(a);
+    System.out.println(result);
   }
 }
