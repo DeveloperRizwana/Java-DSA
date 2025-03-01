a1190b4bc60d475a9452b4ded4a9ee22202bc839
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-01 11:22:17 +0530

    practiced matrix

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,24 +1,37 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 public class fifthPractcie {
-  static ArrayList<Integer> leaders(int arr[]) {
-    int n = arr.length, leaders = arr[n - 1];
-    ArrayList<Integer> result = new ArrayList<>();
-    for (int i = n - 1; i >= 0; i--) {
-      if (arr[i] >= leaders) {
-        leaders = arr[i];
-        result.add(arr[i]);
+  static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  static int[][] transpose(int[][] a, int r, int c) {
+    int[][] result = new int[c][r];
+    for (int i = 0; i < c; i++) {
+      for (int j = 0; j < r; j++) {
+        result[i][j] = a[j][i];
       }
     }
-    Collections.reverse(result);
     return result;
   }
 
   public static void main(String[] args) {
-    int[] a = { 5, 10, 20, 40 };
-    ArrayList<Integer> result = leaders(a);
-    System.out.println(result);
+    int[][] matrix = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 },
+    };
+    System.out.println("Original Matrix:");
+    printMatrix(matrix);
+
+    matrix = transpose(matrix, 4, 4);
+
+    System.out.println("Transpose Matrix:");
+    printMatrix(matrix);
   }
 }
