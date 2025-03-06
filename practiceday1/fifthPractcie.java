273a89a70734e7ada04e312d1c7f8fd3572f35ac
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-06 10:01:30 +0530

    solved a problem on leetcode

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class fifthPractcie {
   static void printMatrix(int[][] matrix) {
@@ -10,17 +11,32 @@ public class fifthPractcie {
     }
   }
 
-  public static void setZeroes(int[][] matrix) {
+  public static int[][] transpose(int[][] matrix) {
+    int n = matrix.length, m = matrix[0].length;
+    int[][] result = new int[m][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        result[j][i] = matrix[i][j];
+      }
+    }
+    return result;
 
   }
 
   public static void main(String[] args) {
     int[][] a = {
-        { 0, 1, 2, 0 },
-        { 3, 4, 5, 2 },
-        { 1, 3, 1, 5 },
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 },
     };
-    System.out.println("Input Matrix");
+    // int[][] result = transpose(a);
+    // System.out.println(Arrays.toString(result));
+    System.out.println("Input MAtrix");
     printMatrix(a);
+
+    int[][]result = transpose(a);
+
+    System.out.println("output MAtrix");
+    printMatrix(result);
   }
 }
