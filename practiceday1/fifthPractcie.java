31806f340e816b3760060332d0d24192ac0206dc
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-10 09:09:10 +0530

    solved a problem on leetcode

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class fifthPractcie {
   static void printMatrix(int[][] matrix) {
@@ -9,16 +11,38 @@ public class fifthPractcie {
     }
   }
 
-  public static void transpose(int[][] matrix) {
-    int n = matrix.length, m = matrix[0].length;
-    for (int i = 0; i < m; i++) {
-      for (int j = i+1; j < n; j++) {
-
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-      }
+  public static List<Integer> spiralOrder(int[][] matrix) {
+    ArrayList<Integer> result = new ArrayList<>();
+    int m = matrix.length, n = matrix[0].length;
+    int topRow = 0, bottomRow = m - 1;
+    int rightCol = n - 1, leftCol = 0;
+    while (topRow <= bottomRow && leftCol <= rightCol) {
+    // topRow
+    for (int j = leftCol; j <= rightCol; j++) {
+      result.add(matrix[topRow][j]);
     }
+    topRow++;
+
+    // rightCol-> topRow to bottomRow
+    for (int i = topRow; i <= bottomRow; i++) {
+      result.add(matrix[i][rightCol]);
+    }
+    rightCol--;
+
+    // bottomRow -> rightCol to leftCol
+    for (int j = rightCol; j >= leftCol; j--) {
+      result.add(matrix[bottomRow][j]);
+    }
+    bottomRow--;
+
+    // leftCol -> bottomRow to topRow
+    for (int i = bottomRow; i >= topRow; i--) {
+      result.add(matrix[i][leftCol]);
+    }
+    leftCol++;
+  }
+    return result;
+
   }
 
   public static void main(String[] args) {
@@ -31,9 +55,7 @@ public class fifthPractcie {
     printMatrix(a);
 
     // int[][]result = transpose(a);
-
-    System.out.println("Output MAtrix: ");
-    transpose(a);
-    printMatrix(a);
+    List<Integer> result = spiralOrder(a);
+    System.out.println("Output MAtrix: " + result);
   }
 }
