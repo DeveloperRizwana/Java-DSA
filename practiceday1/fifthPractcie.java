a80b13b04487d2a7a6a9504c5bb497a7916387c4
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-03-13 20:17:28 +0530

    practiced one problem

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -12,46 +12,53 @@ public class fifthPractcie {
   }
 
   public static List<Integer> spiralOrder(int[][] matrix) {
+
     ArrayList<Integer> result = new ArrayList<>();
     int m = matrix.length, n = matrix[0].length;
     int topRow = 0, bottomRow = m - 1;
     int rightCol = n - 1, leftCol = 0;
+
     while (topRow <= bottomRow && leftCol <= rightCol) {
-    // topRow
-    for (int j = leftCol; j <= rightCol; j++) {
-      result.add(matrix[topRow][j]);
-    }
-    topRow++;
+      // topRow
+      for (int j = leftCol; j <= rightCol; j++) {
+        result.add(matrix[topRow][j]);
+      }
+      topRow++;
 
-    // rightCol-> topRow to bottomRow
-    for (int i = topRow; i <= bottomRow; i++) {
-      result.add(matrix[i][rightCol]);
-    }
-    rightCol--;
+      // rightCol-> topRow to bottomRow
+      for (int i = topRow; i <= bottomRow; i++) {
+        result.add(matrix[i][rightCol]);
+      }
+      rightCol--;
 
-    // bottomRow -> rightCol to leftCol
-    for (int j = rightCol; j >= leftCol; j--) {
-      result.add(matrix[bottomRow][j]);
-    }
-    bottomRow--;
+      // bottomRow -> rightCol to leftCol
+      if (topRow <= bottomRow) { // Only if still within bounds
+        for (int j = rightCol; j >= leftCol; j--) {
+          result.add(matrix[bottomRow][j]);
+        }
+        bottomRow--;
+      }
 
-    // leftCol -> bottomRow to topRow
-    for (int i = bottomRow; i >= topRow; i--) {
-      result.add(matrix[i][leftCol]);
+      // leftCol -> bottomRow to topRow
+      if (leftCol <= rightCol) { // Only if still within bounds
+        for (int i = bottomRow; i >= topRow; i--) {
+          result.add(matrix[i][leftCol]);
+
+        }
+        leftCol++;
+      }
     }
-    leftCol++;
-  }
     return result;
 
   }
 
   public static void main(String[] args) {
     int[][] a = {
-        { 1, 2, 3 },
-        { 4, 5, 6 },
-        { 7, 8, 9 },
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
     };
-    System.out.println("Input MAtrix");
+    System.out.println("Input MAtrix :");
     printMatrix(a);
 
     // int[][]result = transpose(a);
