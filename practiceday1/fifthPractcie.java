6e50f1cf172db69a3caa9754dc115f8e317ab4cc
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-11 08:14:37 +0530

    practiced a problem on leetcode

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class fifthPractcie {
   static void printMatrix(int[][] matrix) {
     for (int i = 0; i < matrix.length; i++) {
@@ -11,27 +8,22 @@ public class fifthPractcie {
     }
   }
 
-  public static int search(int[] nums, int target) {
-    int n = nums.length;
-    int st = 0, end = n - 1;
-    while (st <= end) {
-      int mid = st + (end - st) / 2;
-      if (target == nums[mid]) {
-        return mid;
-      } else if (target > nums[mid]) {
-        st = mid + 1;
-      } else {
-      
-        end = mid - 1;
+  public static boolean check(int[] nums) {
+    int n = nums.length, count = 0;
+    for (int i = 1; i < n; i++) {
+      if (nums[i - 1] > nums[i]) {
+        count++;
       }
       
     }
-    return -1;
+    if (nums[nums.length - 1] > nums[0]) {
+      count++;  
+    }
+    return count <= 1;
   }
 
   public static void main(String[] args) {
-    int[] a = { -1,0,3,5,9,12 };
-    int result = search(a, 2);
-    System.out.println("Result : " + result);
+    int[] a = { 3, 4, 5, 1, 2 };
+    System.out.println(check(a));
   }
 }
