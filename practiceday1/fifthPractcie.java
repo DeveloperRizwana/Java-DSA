c564beb86a0ed98a67fdf4fa26d854ea18abf74f
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-12 08:14:52 +0530

    practiced a problem on leet code

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -8,22 +8,20 @@ public class fifthPractcie {
     }
   }
 
-  public static boolean check(int[] nums) {
-    int n = nums.length, count = 0;
-    for (int i = 1; i < n; i++) {
-      if (nums[i - 1] > nums[i]) {
-        count++;
+  public static int removeDuplicates(int[] nums) {
+    int x = 0, n = nums.length;
+    for (int i = 0; i < n - 1; i++) {
+      if (nums[i] != nums[i + 1]) {
+        nums[x++] = nums[i];
       }
-      
     }
-    if (nums[nums.length - 1] > nums[0]) {
-      count++;  
-    }
-    return count <= 1;
+    nums[x++] = nums[n - 1];
+    return x;
+
   }
 
   public static void main(String[] args) {
-    int[] a = { 3, 4, 5, 1, 2 };
-    System.out.println(check(a));
+    int[] a = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+    System.out.println(removeDuplicates(a));
   }
 }
