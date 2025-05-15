75fd416276fc0a679d6ee4b8f89ac73d1610e76c
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-15 07:18:49 +0530

    practiced one problem on geeksforgeeks

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -8,15 +8,21 @@ public class fifthPractcie {
     }
   }
 
-  public static void moveZeroes(int[] nums) {
-    int n = nums.length, x = 0;
+  public static int secondLargestElement(int[] nums) {
+    int max = Integer.MIN_VALUE, secondMax = -1, n = nums.length;
     for (int i = 0; i < n; i++) {
-      if (nums[i] != 0) {
-        swap(nums, i, x);
-        x++;
+      if (nums[i] > max) {
+        max = nums[i];
       }
     }
-
+    for (int i = 0; i < n; i++) {
+      if (nums[i] == max) {
+        nums[i] = -1;
+      } else if (nums[i] > secondMax) {
+        secondMax = nums[i];
+      } 
+    }
+    return secondMax;
   }
 
   public static void swap(int[] arr, int st, int end) {
@@ -26,10 +32,10 @@ public class fifthPractcie {
   }
 
   public static void main(String[] args) {
-    int[] a = { 1,0,1 };
-    moveZeroes(a);
-    for (int i = 0; i < a.length; i++) {
-      System.out.print(a[i] + " ");
-    }
+    int[] a = { 10,10,10,10};
+    System.out.println(secondLargestElement(a));
+    // for (int i = 0; i < a.length; i++) {
+    // System.out.print(a[i] + " ");
+    // }
   }
 }
