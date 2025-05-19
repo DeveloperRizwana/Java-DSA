508e8bdf7fd98f92e5518a49e2578e7eceef32bc
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-19 09:14:57 +0530

    practiced

--- Array/uniqueElement.java
+++ Array/uniqueElement.java
@@ -1,73 +1,81 @@
-import java.util.Scanner;
 public class uniqueElement {
-    static int findUniqueElement(int[] nums) {
-        // approach-1
-        if(nums.length == 1 && nums[0] == -1) return -1;
-        int n = nums.length;
-        int singleNum = 0;
-        for (int i = 0; i < n; i++) {
-          for (int j = i+1; j < n; j++) {
-            if (nums[i] == nums[j]) {
-              nums[i] = 0;
-              nums[j]= 0;
-            }
-          }
-          if (nums[i] != 0) {
-            singleNum = nums[i];
-          }
-        }
-        return singleNum;
+  static int findUniqueElement(int[] nums) {
+    // approach-1
+    // if (nums.length == 1 && nums[0] == -1)
+    //   return -1;
+    // int n = nums.length;
+    // int singleNum = 0;
+    // for (int i = 0; i < n; i++) {
+    //   for (int j = i + 1; j < n; j++) {
+    //     if (nums[i] == nums[j]) {
+    //       nums[i] = 0;
+    //       nums[j] = 0;
+    //     }
+    //   }
+    //   if (nums[i] != 0) {
+    //     singleNum = nums[i];
+    //   }
+    // }
+    // return singleNum;
 
-        // // approach-2
-        // int n = nums.length;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i + 1; j < n; j++) {
-        //         if (nums[i] == nums[j]) {
-        //             nums[i] = -1;
-        //             nums[j] = -1;
-        //         }
-        //     }
-        // }
-        // int ans = -1;
-        // for (int i = 0; i < n; i++) {
-        //     if (nums[i] > 0) {
-        //         ans = nums[i];
-        //     }
-        // }
-        // return ans;
+    // // approach-2
+    // int n = nums.length;
+    // for (int i = 0; i < n; i++) {
+    // for (int j = i + 1; j < n; j++) {
+    // if (nums[i] == nums[j]) {
+    // nums[i] = -1;
+    // nums[j] = -1;
+    // }
+    // }
+    // }
+    // int ans = -1;
+    // for (int i = 0; i < n; i++) {
+    // if (nums[i] > 0) {
+    // ans = nums[i];
+    // }
+    // }
+    // return ans;
 
-        //approach-3
-        // int n = nums.length;
-        // int[] result = new int[n];
-        // for(int i = 0; i < n; i++){
-        //     int count = 0; 
-        //     for(int j =0 ; j < n; j++){
-        //         if(nums[i] == nums[j]){
-        //             count++;
-        //         }
-        //     }
-        //     result[i] = count;
-        // }
-        // for(int i = 0; i < n; i++){
-        //     if(result[i] == 1){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
+    // approach-3
+    // int n = nums.length;
+    // int[] result = new int[n];
+    // for(int i = 0; i < n; i++){
+    // int count = 0;
+    // for(int j =0 ; j < n; j++){
+    // if(nums[i] == nums[j]){
+    // count++;
+    // }
+    // }
+    // result[i] = count;
+    // }
+    // for(int i = 0; i < n; i++){
+    // if(result[i] == 1){
+    // return nums[i];
+    // }
+    // }
+    // return -1;
 
+    // approach - 4
+    int n = nums.length;
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+      result ^= nums[i];
     }
+    return result;
+  }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter " + n + " elements ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Unique element: " + findUniqueElement(arr));
+  public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int n = sc.nextInt();
+    // int[] arr = new int[n];
+    // System.out.println("Enter " + n + " elements ");
+    // for (int i = 0; i < n; i++) {
+    //   arr[i] = sc.nextInt();
+    // }
+    int a = [2,2,1];
+    System.out.println("Unique element: " + findUniqueElement(arr));
 
-    }
+  }
 }
 // find the unique number in a given array where all the elements are being
 // repeated twice with one value being unique
