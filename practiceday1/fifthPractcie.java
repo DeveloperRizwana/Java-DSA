76a3ff741146c12798fda5edf8a88b068ddc0ca8
DeveloperRizwana <dev.rizwana.perween01@gmail.com>
2025-05-19 08:41:49 +0530

    practied

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -10,18 +10,25 @@ public class fifthPractcie {
     }
   }
 
-  public static int findMaxConsecutiveOnes(int[] nums) {
-    int n = nums.length, max = 0, maxOne = 0;
+  public static int singleNumber(int[] nums) {
+    int n = nums.length;
     for (int i = 0; i < n; i++) {
-      if (nums[i] == 1) {
-        max++;
-        maxOne = Math.max(maxOne, max);
-      } else {
-        max = 0;
+      for (int j = i + 1; j < n; j++) {
+        if (nums[i] == nums[j]) {
+          nums[i] = 0;
+          nums[j] = 0;
+        }
+      }
+      if (nums[i] != 0) {
+        return nums[i];
       }
     }
-    return maxOne;
-
+    // for (int i = 0; i < n; i++) {
+    // if (nums[i] != 0) {
+    // return nums[i];
+    // }
+    // }
+    return 0;
   }
 
   public static void swap(int[] arr, int st, int end) {
@@ -31,7 +38,7 @@ public class fifthPractcie {
   }
 
   public static void main(String[] args) {
-    int[] a = { 1 };
-    System.out.println(findMaxConsecutiveOnes(a));
+    int[] a = { 1, 0, 1 };
+    System.out.println(singleNumber(a));
   }
 }
