1159f2ba91a982f35216b885041311dcf70b2fb4
Rizwana Perween <dev.rizwana.perween01@gmail.com>
2025-06-24 14:44:25 +0530

    modified

--- practiceday1/fifthPractcie.java
+++ practiceday1/fifthPractcie.java
@@ -38,6 +38,7 @@ public class fifthPractcie {
   public static void main(String[] args) {
     int[] a = { 2,0,1 };
     sortColors(a);
+    
     for (int i = 0; i < a.length; i++) {
       System.out.print(a[i] + " ");
     }
